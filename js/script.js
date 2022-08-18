8041b074feec4e6e79bbe3cee53473e5256aeaf6
Bruno Pessoa <brunochrispim123@gmail.com>
2022-08-18 15:32:19 -0300

    js/plugin

---
+++ js/script.js
@@ -0,0 +1,42 @@
+//Ativar itens do Orçamento
+
+const parametros = new URLSearchParams(location.search);
+
+function ativarProduto(parametro) {
+  const elemento = document.getElementById(parametro);
+  if (elemento) {
+    elemento.checked = true;
+  }
+}
+
+parametros.forEach(ativarProduto);
+
+//---------------------------------------------
+
+//Perguntas Frequents
+
+const perguntas = document.querySelectorAll(".perguntas button");
+
+function ativarPergunta(event) {
+  const pergunta = event.currentTarget;
+  const controls = pergunta.getAttribute("aria-controls");
+  const resposta = document.getElementById(controls);
+
+  resposta.classList.toggle("ativa");
+  const ativa = resposta.classList.contains("ativa");
+  pergunta.setAttribute("aria-expanded", ativa);
+}
+
+function eventosPerguntas(pergunta) {
+  pergunta.addEventListener("click", ativarPergunta);
+}
+
+perguntas.forEach(eventosPerguntas);
+
+//---------------------------------------------------------
+
+//    Animação
+
+if (window.SimpleAnime) {
+  new SimpleAnime();
+}
